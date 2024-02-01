7a3b698575006a3f5e597ab627588f8184eefc9d
Pedro Almeida Carvalho <cont.pacarvalho@gmail.com>
2024-02-01 16:54:22 -0300

    req 4

--- src/routers/login.router.js
+++ src/routers/login.router.js
@@ -3,20 +3,21 @@ const express = require('express');
 const router = express.Router();
 
 const validateLogin = (req, res, next) => {
-  const requiredProperties = ['email', 'password'];
-
-  const hasProperties = requiredProperties.every((property) => property in req.body);
-
-  if (!hasProperties) return res.status(400).json({ message: 'Invalid data' });
+  if (!('email' in req.body)) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' }); 
+  }
+  if (!('password' in req.body)) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' }); 
+  }
 
   const { email, password } = req.body;
 
   const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
   if (!emailRegex.test(email)) {
-    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
-  if (password.lenght <= 6) {
-    return res.status(400).json({ message: 'Password must be more than 6 characters' });
+  if (password.length <= 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
   next();
 };
